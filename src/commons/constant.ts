c8b3ee2726dec430e285695f14687db4722a1ae3
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2023-02-26 17:52:32 -0800

    v1_reality fix regex in unit name (#72)
    * v1_misc fix #47
    
    * v1_misc error logging when update customer
    
    * v1_misc improve error logging
    
    * v1_misc implement validation error message
    
    * v1_misc throw error when re-assign on-doing task
    
    * v1_misc refactor dto & add manual code
    
    * v1_misc report manual code
    
    * v1_misc sell price suggest
    
    * v1_misc update employee account
    
    * v1_unit improve customer & vendor config view (#61)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement format (#63)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement formatting
    
    * fix format
    
    * v1_unit implement product units (#65)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement formatting
    
    * fix format
    
    * v1_unit implement product units
    
    * v1_unit reformat unit
    
    * v1_reality cleanup legacy logic flow (#67)
    
    * v1_reality cleanup legacy logic flow
    
    * v1_reality fix format
    
    * v1_reality fix regex & simplify unit ratio (#69)
    
    * v1_reality cleanup legacy logic flow
    
    * v1_reality fix format
    
    * v1_reality fix regex & simplify ratio
    
    * v1_reality fix regex in unit name (#71)
    
    * v1_reality cleanup legacy logic flow
    
    * v1_reality fix format
    
    * v1_reality fix regex & simplify ratio
    
    * v1_reality fix regex for unit name

--- src/commons/constant.ts
+++ src/commons/constant.ts
@@ -1,3 +1,3 @@
-export const GENERAL_TEXT_REGEX = /[!$%^\*\_+<>?:"{}\[\];/\t]/;
+export const GENERAL_TEXT_REGEX = /[!$%^\*\_+_<>?:"{}\[\];/\t]/;
 export const NUMBER_REGEX = /^[0-9]*$/;
 export const FRACTION_REGEX = /^[0-9/]*$/;
