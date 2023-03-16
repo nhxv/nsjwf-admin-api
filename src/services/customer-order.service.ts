db0c6d3c9e9bf22eca41a6db78ca01fee3408bd9
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2023-03-15 23:01:34 -0700

    v1_misc fix format

--- src/services/customer-order.service.ts
+++ src/services/customer-order.service.ts
@@ -36,7 +36,7 @@ export const findDailyCustomerOrder = async () => {
             NOT: {
               status: OrderStatus.COMPLETED,
             },
-          },          
+          },
         ],
       },
     });
