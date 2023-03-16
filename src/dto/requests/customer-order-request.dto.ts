db16bc02d4bbff494df6f60719de8a4cfa9db552
Xuân Vinh <ngohoangxuanvinh1995@gmail.com>
2023-03-16 09:32:15 -0700

    v1_feedback allow price to be 0 before complete order (#96)
    * v1_misc fix #47
    
    * v1_misc error logging when update customer
    
    * v1_misc improve error logging
    
    * v1_misc implement validation error message
    
    * v1_misc throw error when re-assign on-doing task
    
    * v1_misc refactor dto & add manual code
    
    * v1_misc report manual code
    
    * v1_misc sell price suggest
    
    * v1_misc update employee account
    
    * v1_unit improve customer & vendor config view (#61)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement format (#63)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement formatting
    
    * fix format
    
    * v1_unit implement product units (#65)
    
    * v1_unit update product config view
    
    * v1_unit improve customer & vendor config view
    
    * v1_unit implement formatting
    
    * fix format
    
    * v1_unit implement product units
    
    * v1_unit reformat unit
    
    * v1_reality cleanup legacy logic flow (#67)
    
    * v1_reality cleanup legacy logic flow
    
    * v1_reality fix format
    
    * v1_reality fix regex & simplify unit ratio (#69)
    
    * v1_reality cleanup legacy logic flow
    
    * v1_reality fix format
    
    * v1_reality fix regex & simplify ratio
    
    * v1_reality fix regex in unit name (#71)
    
    * v1_reality cleanup legacy logic flow
    
    * v1_reality fix format
    
    * v1_reality fix regex & simplify ratio
    
    * v1_reality fix regex for unit name
    
    * v1_reality fix unit name validation (#73)
    
    * v1_reality cleanup legacy logic flow
    
    * v1_reality fix format
    
    * v1_reality fix regex & simplify ratio
    
    * v1_reality fix regex for unit name
    
    * v1_reality fix unit name validation
    
    * v1_reality fix format
    
    * v1_payment implement customer payment (#75)
    
    * v1_payment setup payment status
    
    * v1_payment implement customer payment
    
    * v1_payment fix format
    
    * v1_payment process test order payment (#77)
    
    * v1_payment setup payment status
    
    * v1_payment implement customer payment
    
    * v1_payment fix format
    
    * v1_payment process test order payment
    
    * v1_payment fix format
    
    * v1_payment fix update customer order (#81)
    
    * v1_payment setup payment status
    
    * v1_payment implement customer payment
    
    * v1_payment fix format
    
    * v1_payment process test order payment
    
    * v1_payment fix format
    
    * v1_payment fix #80
    
    * v1_payment reset db (#84)
    
    * v1_payment setup payment status
    
    * v1_payment implement customer payment
    
    * v1_payment fix format
    
    * v1_payment process test order payment
    
    * v1_payment fix format
    
    * v1_payment fix #80
    
    * v1_reset update reset db
    
    * v1_reset reset stock when nuke configure data
    
    * v1_payment fix refund deduction
    
    * v1_misc fix stock flow
    
    * v1_misc fix stock flow
    
    * v1_misc update employee credentials
    
    * v1_misc fix format
    
    * v1_misc fix order time
    
    * v1_misc fix format
    
    * v1_payment revert customer order
    
    * v1_payment fix format
    
    * v1_misc fix decimal rounding
    
    * v1_misc fix format
    
    * v1_feedback prepare for new order view
    
    * v1_misc fix format
    
    * v1_feedback allow price to be 0 before complete order

--- src/dto/requests/customer-order-request.dto.ts
+++ src/dto/requests/customer-order-request.dto.ts
@@ -30,7 +30,7 @@ export const customerOrderSchema = Joi.object<CustomerOrderRequestDto>({
         .regex(GENERAL_TEXT_REGEX, { invert: true }),
       quantity: Joi.number().integer().positive().required(),
       unitCode: Joi.string().trim().max(21).required(),
-      unitPrice: Joi.number().positive().required(),
+      unitPrice: Joi.number().min(0).required(),
       id: Joi.number().integer().positive(),
       orderCode: Joi.string()
         .max(20)
