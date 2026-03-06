ad4a5d4b69d255b601004260c5dd9e679751a356
MikeJollie2707 <bachngo@yahoo.com>
2026-03-06 15:02:22 -0800

    Minor changes

--- container/postgresql/scripts/initdb.sh
+++ container/postgresql/scripts/initdb.sh
@@ -4,6 +4,6 @@ set -e
 APP_PASSWORD="$(tr -d '\r\n' < /run/secrets/db-app-password)"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER nsjwfbackend WITH CREATEDB PASSWORD '$(APP_PASSWORD)';
-    CREATE DATABASE defaultdb WITH OWNER nsjwfbackend;
+    CREATE USER "$PG_USER" WITH CREATEDB PASSWORD '$APP_PASSWORD';
+    CREATE DATABASE "$PG_DATABASE" WITH OWNER "$PG_USER";
 EOSQL
